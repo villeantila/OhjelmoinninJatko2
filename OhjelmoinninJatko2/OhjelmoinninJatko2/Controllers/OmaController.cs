0e0165103ff93f04b78fe2c9da294915b0b84daa
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-08-11 23:23:49 +0300

    Lisätty tietokanta.

---
+++ OhjelmoinninJatko2/OhjelmoinninJatko2/Controllers/OmaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OhjelmoinninJatko2.Controllers
+{
+    public class OmaController : Controller
+    {
+        // GET: Oma
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
