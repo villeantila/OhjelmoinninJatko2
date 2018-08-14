d5ec8be93eed1ec629c9a9b299f11a81d81a1ea2
Ville Antila <ville.antila@stud.pointcollege.fi>
2018-08-14 16:44:13 +0300

    Lisätty Henkilöt-näkymä, Id ei näy kuin vanhassa mallissa (/henkilotvanha).

--- OhjelmoinninJatko2/OhjelmoinninJatko2/Controllers/OmaController.cs
+++ OhjelmoinninJatko2/OhjelmoinninJatko2/Controllers/OmaController.cs
@@ -16,7 +16,7 @@ namespace OhjelmoinninJatko2.Controllers
             return View();
         }
 
-        public JsonResult GetList()
+        public JsonResult GetListProjektit()
         {
             OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
             
@@ -53,7 +53,7 @@ namespace OhjelmoinninJatko2.Controllers
             return Json(json, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Update(Projektit proj)
+        public ActionResult UpdateProjekti(Projektit proj)
         {
             OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
             int id = proj.ProjektiId;
@@ -98,7 +98,7 @@ namespace OhjelmoinninJatko2.Controllers
             return Json(OK, JsonRequestBehavior.AllowGet);
         }  
         
-        public ActionResult Delete(int id)
+        public ActionResult DeleteProjekti(int id)
         {
             OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
 
@@ -119,6 +119,144 @@ namespace OhjelmoinninJatko2.Controllers
             entities.Dispose();
 
             return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
+
+        //*******************************************************************************
+
+        // Henkilot-taulun osuus alkaa
+
+
+        public ActionResult Henkilot()
+        {
+            return View();
+        }
+
+        public JsonResult GetListHenkilot()
+        {
+            OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
+
+            var model = (from c in entities.Henkilot
+                         select new
+                         {
+                             HenkiloId = c.HenkiloId,
+                             Identity = c.Identity,
+                             Etunimi = c.Etunimi,
+                             Sukunimi = c.Sukunimi,
+                             Osoite = c.Osoite,
+                             Esimies = c.Esimies
+                         }).ToList();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetSingleHenkilo(int id)
+        {
+            OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
+
+            var model = (from c in entities.Henkilot
+                         where c.HenkiloId == id
+                         select new
+                         {
+                             HenkiloId = c.HenkiloId,
+                             Identity = c.Identity,
+                             Etunimi = c.Etunimi,
+                             Sukunimi = c.Sukunimi,
+                             Osoite = c.Osoite,
+                             Esimies = c.Esimies
+                         }).FirstOrDefault();
+
+            string json = JsonConvert.SerializeObject(model);
+            entities.Dispose();
+
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult UpdateHenkilo(Henkilot henk)
+        {
+            OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
+            int id = henk.HenkiloId;
+
+            bool OK = false;
+
+            // onko kyseessä muokkaus vai uuden lisääminen?
+            if (id == 0)
+            {
+                // kyseessä on uuden henkilön lisääminen, kopioidaan kentät
+                Henkilot dbItem = new Henkilot()
+                {
+                    Identity = henk.Identity,
+                    Etunimi = henk.Etunimi,
+                    Sukunimi = henk.Sukunimi,
+                    Osoite = henk.Osoite,
+                    Esimies = henk.Esimies
+                };
+
+                // tallennus tietokantaan, Henkilö-Id muodostuu automaattisesti
+                entities.Henkilot.Add(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+
+            else
+            {
+                // muokkaus, haetaan id:n perusteella riviä tietokannasta
+                Henkilot dbItem = (from c in entities.Henkilot
+                                    where c.HenkiloId == id
+                                    select c).FirstOrDefault();
+                if (dbItem != null)
+                {
+                    dbItem.Identity = henk.Identity;
+                    dbItem.Etunimi = henk.Etunimi;
+                    dbItem.Sukunimi = henk.Sukunimi;
+                    dbItem.Osoite = henk.Osoite;
+                    dbItem.Esimies = henk.Esimies;
+
+                    // tallennus tietokantaan
+                    entities.SaveChanges();
+                    OK = true;
+                }
+            }
+
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DeleteHenkilo(int id)
+        {
+            OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
+
+            bool OK = false;
+
+            Henkilot dbItem = (from c in entities.Henkilot
+                               where c.HenkiloId == id
+                               select c).FirstOrDefault();
+
+            if (dbItem != null)
+            {
+                // poisto tietokannasta
+                entities.Henkilot.Remove(dbItem);
+                entities.SaveChanges();
+                OK = true;
+            }
+
+            entities.Dispose();
+
+            return Json(OK, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult henkilotvanha()
+        {
+            OhjelmoinninJatkoEntities entities = new OhjelmoinninJatkoEntities();
+            List<Henkilot> model = entities.Henkilot.ToList();
+            entities.Dispose();
+            return View(model);
         }
     }
 }
